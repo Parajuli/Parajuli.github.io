39302f44cd9ec11952489f934be07681bfbabc74
Rajiv Parajuli <raziv.parazuli@gmail.com>
2018-06-05 09:14:22 +0545

    Update custom.js

--- js/custom.js
+++ js/custom.js
@@ -136,7 +136,7 @@ jQuery(document).ready(function(){
 	// Needed variables
 	var $map 				= $('#map'),
 		$tabContactClass 	= ('tab-contact'),
-		$address 			= ('Kapan Aakase Dhara, Budhanilkantha');
+		$address 			= 'Kapan Aakase Dhara, Budhanilkantha 44600 Nepal';
 	
 	$content.bind('easytabs:after', function(evt,tab,panel) {
 		if ( tab.hasClass($tabContactClass) ) {
